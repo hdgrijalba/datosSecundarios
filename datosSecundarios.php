214556258f5724a0ac2476f1b74677169a370d9f
hdgrijalba <hdgrijalba@gmail.com>
2015-05-28 13:16:58 -0500

    inicio
    prueba

---
+++ datosSecundarios.php
@@ -0,0 +1,12 @@
+<?php
+namespace datosSecundarios;
+use yii\web\Controller;
+class datosSecundarios extends Controller
+{
+	public function actionConfiguration()
+	{
+		$app = \Yii::$app;
+		
+		return 'Prueba';
+	}
+}
